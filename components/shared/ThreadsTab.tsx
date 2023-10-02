ea682993891ac772736a00592e606cffa47f22c1
Harikrishnan DS <thisismekrishnan@gmail.com>
2023-10-02 15:12:01 +0530

    adding webhooks form community

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,36 @@
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import ThreadCard from "../cards/ThreadCard";
+
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+async function ThreadsTab({ currentUserId, accountId, accountType }: Props) {
+  let result = await fetchUserPosts(accountId);
+  if (!result) redirect("/");
+  return (
+    <section className=" mt-9 flex flex-col gap-10">
+      {result.threads.map((thread: any) => (
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={currentUserId}
+          parentId={thread.parentId}
+          author={
+            accountType === "User"
+                ?{name:result.name,image:result.image,id:result.id}
+                :{name:thread.name,image:thread.image,id:thread.id}
+          }
+          content={thread.text}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default ThreadsTab;
